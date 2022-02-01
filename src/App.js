f0cbebb09fb5ffa95dcd6c22a8626effc50f36be
paulsalamone <psalamone@gmail.com>
2022-02-01 17:00:57 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>POKE-FIGHT!</h1>
+      <h2>GROUP TWO VERSION</h2>
+    </div>
+  );
+}
+
+export default App;
