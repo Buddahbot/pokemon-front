3301e75e0e74c19efc12909a00c4fc100942e9a2
jcwalina <joannacwalinaa@gmail.com>
2022-02-08 21:55:06 +0100

    Leaderboard Button added to the winner.js

--- src/components/Winner.js
+++ src/components/Winner.js
@@ -2,6 +2,7 @@ import "./winnerStyle.css";
 import { useContext } from "react";
 import { PokeContext } from "../context/PokeContext";
 import party from "party-js";
+import {NavLink} from 'react-router-dom' 
 
 const Winner = () => {
 
@@ -22,23 +23,28 @@ const Winner = () => {
 
   return (
     <div className="container ">
-    
-      <h2>Winner</h2>
+      <div className="buttons">
+            <NavLink className="leaderboardButton" to="/leaderboard">Leaderboard</NavLink>
+      </div>
+        
+          <h2>WINNER</h2>
 
-      <div className="winnerPoke">
+       <div className="winnerPoke">
 
-            <div className="">
-            <img src={placeholderImage} className="img"/>
-            </div>
+                <div className="">
+                <img src={placeholderImage} className="img"/>
+                </div>
 
-            <button onClick={onClick}>Click me!</button>
+          <button onClick={onClick}>Click me!</button>
 
-        <div className="pokeName">
-            <h3 >{placeholder.name.english}</h3>
-        <h3 >{placeholder.name.japanese}</h3>
-        </div>
+          <div className="info">
+          <div className="pokeName">
+              <h3 >{placeholder.name.english}</h3>
+                <h3 >{placeholder.name.japanese}</h3>
+          </div>
+          </div>
 
-</div>
+      </div>
     </div>
   );
 };
