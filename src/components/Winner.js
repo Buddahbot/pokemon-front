4c9505ee31ec4daa6d47825905beb3d266c72c03
jcwalina <joannacwalinaa@gmail.com>
2022-02-11 17:19:08 +0100

    winner page updated

--- src/components/Winner.js
+++ src/components/Winner.js
@@ -2,7 +2,8 @@ import "./winnerStyle.css";
 import { useContext } from "react";
 import { PokeContext } from "../context/PokeContext";
 import party from "party-js";
-import {NavLink} from 'react-router-dom' 
+import { NavLink } from 'react-router-dom'
+
 
 const Winner = () => {
 
@@ -11,7 +12,7 @@ const Winner = () => {
   const placeholder = pokemons[11];
 
   const ID = placeholder.id;
-  
+
   console.log(ID);
   const placeholderImage = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${ID}.png`;
 
@@ -24,29 +25,36 @@ const Winner = () => {
   return (
     <div className="container ">
       <div className="buttons">
-            <NavLink className="button" to="/leaderboard">Leaderboard</NavLink>
+        <NavLink className="button" to="/leaderboard">Leaderboard</NavLink>
       </div>
-        
-          <h2>WINNER</h2>
 
-       <div className="winnerPoke">
+      <div class="waviy">
+        <span className="--i:2">W</span>
+        <span className="--i:3">I</span>
+        <span className="--i:4">N</span>
+        <span className="--i:5">N</span>
+        <span className="--i:6">E</span>
+        <span className="--i:7">R</span>
+      </div>
 
-                <div className="">
-                <img src={placeholderImage} className="img"/>
-                </div>
+      <div className="winnerPoke">
 
-          <button className="button" onClick={onClick}>Click me!</button>
+        <div className="">
+          <img src={placeholderImage} className="img" />
+        </div>
 
-          <div className="playAgainButton">
-            <NavLink className="button" to="/">PLAY AGAIN</NavLink>
-          </div>
+        <button className="button" onClick={onClick}>Click me!</button>
 
-          <div className="info">
+        <div className="playAgainButton">
+          <NavLink className="button" to="/">PLAY AGAIN</NavLink>
+        </div>
+
+        <div className="info">
           <div className="pokeNames">
-              <h3 >{placeholder.name.english}</h3>
-                <h3 >{placeholder.name.japanese}</h3>
+            <h3 >{placeholder.name.english}</h3>
+            <h3 >{placeholder.name.japanese}</h3>
           </div>
-          </div>
+        </div>
 
       </div>
     </div>
