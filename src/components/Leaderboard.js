de9503a1b5ccc5a49e3c232989f168ddacede91d
jcwalina <joannacwalinaa@gmail.com>
2022-02-11 21:11:23 +0100

    Leaderboard not done

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,11 +1,43 @@
 import "../App.css";
+import "./leaderStyle.css"
 
 const Leaderboard = () => {
   return (
-    <>
+    <div className="container-leader">
       <h1>Leaderboard</h1>
-      <p>data goes here</p>
-    </>
+      <div className="table">
+
+        <div className="category-name">
+          <h1 className="h1">Player Name</h1>
+          <h1 className="h1">Games Played</h1>
+          <h1 className="h1">Games Won</h1>
+
+
+
+        </div>
+
+
+
+        <div>
+          <div className="name">
+
+          </div>
+
+          <div className="played-games">
+
+          </div>
+
+          <div className="won-games">
+
+          </div>
+
+        </div>
+
+
+      </div>
+
+
+    </div>
   );
 };
 
