753ffe5ddf91dcce0ce299d688df881af58755c0
jcwalina <joannacwalinaa@gmail.com>
2022-02-12 17:48:15 +0100

    search input done + css for all inputs

--- src/components/Login.js
+++ src/components/Login.js
@@ -31,14 +31,16 @@ const Login = () => {
           <h2>Login:</h2>
           <form onSubmit={testLogin}>
             <input
+              className="form-control mr-sm-2 input-search"
               type="text"
-              placeholder="enter Playername"
+              placeholder="enter playername"
               value={name}
               name="Playername"
               onChange={(e) => setName(e.target.value)}
             ></input>
-            
+
             <input
+              className="form-control mr-sm-2 input-search"
               placeholder="enter password"
               value={password}
               type="password"
